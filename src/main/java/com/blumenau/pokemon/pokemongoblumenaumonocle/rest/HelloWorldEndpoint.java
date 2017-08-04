d88e4a2e4cab708fbc5f8e8785fed5cd785eeecb
Mauricio Narcizo <mauricionarcizo@MacBook-Air-de-Mauricio.local>
2017-08-04 14:14:30 -0300

    get address by pokemon

--- src/main/java/com/blumenau/pokemon/pokemongoblumenaumonocle/rest/HelloWorldEndpoint.java
+++ src/main/java/com/blumenau/pokemon/pokemongoblumenaumonocle/rest/HelloWorldEndpoint.java
@@ -11,6 +11,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import com.blumenau.pokemon.pokemongoblumenaumonocle.rest.HelloWorldEndpoint.GoogleMapsAddress.Results;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -29,6 +30,8 @@ import javax.ws.rs.QueryParam;
 @Stateless
 public class HelloWorldEndpoint {
 	private String urlServer = "http://158.69.250.59:5555/data";
+    private String urlGoogleMapsAddress = "http://maps.googleapis.com/maps/api/geocode/json";
+    // http://maps.googleapis.com/maps/api/geocode/json?latlng=-26.867082,-49.103068&sensor=true get address by lat long
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response doGet(@QueryParam("lastId") String lastId) throws Exception{
@@ -41,12 +44,39 @@ public class HelloWorldEndpoint {
 
             com.google.gson.Gson gson = new com.google.gson.Gson();
             List<Pokemon> pokemons = gson.fromJson(json, ArrayList.class);
-
             return Response.ok(pokemons).build();
         } catch (Exception ex) {
-			throw ex;
+			ex.printStackTrace();
+            return Response.serverError().build();
         }
-        // return Response.ok("{}").build();
+        
+    }
+
+    private String getAddressFromLatLon(long lat, long lon){
+        System.out.println("address");
+        urlGoogleMapsAddress += "?latlng=" + lat+','+lon;
+		try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpGet request = new HttpGet(urlGoogleMapsAddress);
+            request.addHeader("content-type", "application/json");
+            HttpResponse result = httpClient.execute(request);
+            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+
+            com.google.gson.Gson gson = new com.google.gson.Gson();
+            GoogleMapsAddress address = gson.fromJson(json, GoogleMapsAddress.class);
+            Results rs = address.results.get(0);
+            return rs.formatted_address;
+        } catch (Exception ex) {
+			ex.printStackTrace();
+            return "";
+        }
+    }
+
+    public static class GoogleMapsAddress{
+        public List<Results> results;
+
+        public static class Results{
+            public String formatted_address;
+        }
     }
     
     public static class Pokemon{
@@ -64,5 +94,6 @@ public class HelloWorldEndpoint {
         public String move1;
         public String move2;
         public long sta;
+        public String address;
     }
 }
